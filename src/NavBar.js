b8aababab29f38952b724e4c396732f140b2f607
bradsimpson213 <bsimpson@appacademy.io>
2021-01-04 23:59:05 -0500

    started on navbar

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+
+    return (
+        <div className="Navbar">
+            <img src="./assets/images/blue-crew.png" />
+            <h1>Amount Us</h1>
+        </div>
+    )
+};
+
+export default NavBar;
