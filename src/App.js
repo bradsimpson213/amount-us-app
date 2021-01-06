3d7713ffb7f553492ecba16174915bc901945803
bradsimpson213 <bsimpson@appacademy.io>
2021-01-05 22:27:27 -0500

    working on styles for Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import NavBar from "./NavBar";
-import './App.css';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
+// import './App.css';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "blue",
+    height: '100%',
+    width: '100%'
+  }
+});
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-      <NavBar />
-    </div>
+      <div 
+          className={ classes.root }
+      >
+          <NavBar />
+      </div>
   );
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,61 @@
 import React from "react";
-import "./NavBar.css";
+import { makeStyles } from "@material-ui/core/styles";
+import blueCrewImage  from "./assets/images/blue-crew.png";
+import orangeCrewImage  from "./assets/images/orange-crew.png";
+
+const useStyles = makeStyles({
+    root:{
+        backgroundColor: 'black',
+        border: '1px solid black',
+        height: '140px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    navImage: {
+        height: '120px',
+        margin: '0px 15px 0px 15px'
+    },
+    titleContainer: {
+        fontFamily: 'Amoung-us',
+        color: 'white',
+        height: '120px',
+        display: 'flex',
+        flexFlow: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    mainTitle: {
+        fontSize: '80px',
+        marginBottom: '30px'
+    },
+    subTitle: {
+        fontSize: '20px',
+        marginTop: '-40px',
+    }
+});
+
 
 const NavBar = () => {
+    const classes = useStyles();
 
     return (
-        <div className="Navbar">
-            <img src="./assets/images/blue-crew.png" />
-            <h1>Amount Us</h1>
+        <div className={ classes.root }>
+            <img 
+                className={ classes.navImage } 
+                src={ blueCrewImage } 
+                alt="blue crewmate"
+            />
+            <div className={ classes.titleContainer }>
+                <h1 className={ classes.mainTitle }>Amount Us</h1>
+                    <h4 className={ classes.subTitle } >Math "Among Us" Style</h4>
+            </div>
+            <img 
+                className={ classes.navImage } 
+                src={ orangeCrewImage } 
+                alt="orange crewmate"
+            />
         </div>
     )
 };
