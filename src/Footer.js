9dc4235c536de02270721a61837b6f36f3c06fe8
bradsimpson213 <bsimpson@appacademy.io>
2021-01-06 22:14:20 -0500

    styling on footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,15 +2,31 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-
+    root: {
+        flexShrink: '0',
+        color: 'white',
+        textAlign: 'center',
+        margin: '15px 0px 15px 0px'    
+    },
+    label: {
+        fontFamily: 'Amoung-us',
+        fontSize: '28px'
+    },
+    dots: {
+        margin: '0px 10px 0px 10px'
+    }
 })
 
 const Footer = () => {
     const classes = useStyles();
-    
+
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className={ classes.root }>
+            <span className={ classes.dots }>●</span>
+            <span className={ classes.label }>
+                Website Created By Brad and Andy Simpson ©2021
+            </span>
+            <span className={ classes.dots }>●</span>
         </div>
     )
 };
