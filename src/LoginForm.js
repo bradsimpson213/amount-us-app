cc99e774f26401a64e9fe06d909e21e3c1d7f2ab
bradsimpson213 <bsimpson@appacademy.io>
2021-01-08 23:28:09 -0500

    login form styling

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
     root: {
-        color: 'white'
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '30px 0px 0px 0px', 
+    },
+    container: {
+        border: '1px solid white',
+        borderRadius: '15px',
+        height: '600px',
+        width: '500px',
+        color: 'white',
+        textAlign: 'center',
+        fontFamily: 'Amoung-us',
+        fontSize: '30px',
+        marginTop: '40px',
     }
+
 });
 
 const LoginForm = () => {
     const classes = useStyles();
     return (
-        <div>
-            <h1 className={ classes.root }>LOGIN FORM</h1>
+        <div className={ classes.root }>
+            <div className={ classes.container }>
+            <h1>Login</h1>
+                <form 
+                    noValidate 
+                    autoComplete="off"
+                >
+                <TextField 
+                    id="outlined-required" 
+                    label="Username"
+                    required
+                    defaultValue="Username" 
+                />
+                </form> 
+            </div>
         </div>
     )
 };
